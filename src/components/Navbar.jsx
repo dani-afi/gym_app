adffe677a7a27084676735abf9786e78649f0a41
Daniel <daniel.afilipoaie@gmail.com>
2022-08-31 20:50:56 +0300

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { Link, NavLink } from "react-router-dom";
+import { links } from "../data";
+import Logo from "../images/logo.png";
+import { GoThreeBars } from "react-icons/go";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className="logo">
+          <img src={Logo} alt="logo" />
+        </Link>
+        <ul className="nav__links">
+          {links.map(({ name, path, index }) => {
+            return (
+              <li>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) => (isActive ? "active-nav" : "")}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <button className="nav__toggle-btn">
+          <GoThreeBars />
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
